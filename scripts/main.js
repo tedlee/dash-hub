ccb5c0fd4e7c0d74cd088061c08bfd03a6d37a41
Ted Lee <Ted@TedLee.me>
2013-01-12 11:42:25 -0500

    Added relative time stampts using moment.js

--- scripts/main.js
+++ scripts/main.js
@@ -16,13 +16,17 @@ function callGitHub() {
 	var url =  "https://api.github.com/repos/" + TARGET + "/commits";
 	$.getJSON(url, function(data) {
 		var messages = [];
+		var time = [];
 
 		for (var i = 0; i < 3; i++){
-			messages.push(data[i].commit.message)
-			console.log(messages[i])
-			$("#commit-messages").append(messages[i] + "<br />");
+			messages.push(data[i].commit.message);
+			var time = getPrettyTime(data[i].commit.committer.date);
+			$("#commit-messages").append(messages[i] + " — " + time + "<br />");
 		}
-
-
 	});
 }
+
+function getPrettyTime (dirtyDate) {
+	return moment(dirtyDate).fromNow();
+}
+
