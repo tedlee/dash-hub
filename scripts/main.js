2f5e2d5410b34ecd1d8889b694647b09b4b11daf
Ted Lee <Ted@TedLee.me>
2013-01-13 13:05:27 -0500

    Added commits this week field

--- scripts/main.js
+++ scripts/main.js
@@ -16,8 +16,12 @@ function callGitHub() {
 	// Send the request
 	var url =  "https://api.github.com/repos/" + TARGET + "/commits";
 	var commitsToday = 0;
+	var commitsWeekly = 0;
 	var messages = [];
 	var time = [];
+	var now = moment();
+
+	console.log(now.format("w"))
 	
 	$.getJSON(url + "?callback=?", null, function(commits) {
 
@@ -25,13 +29,19 @@ function callGitHub() {
 			messages.push(commits.data[i].commit.message);
 			time.push(commits.data[i].commit.committer.date);
 
-			if ( moment(time[i]).format("MMM Do YY") == moment().format("MMM Do YY") ){
+			if ( moment(time[i]).format("MMM Do YY") == now.format("MMM Do YY") ){
 				commitsToday += 1
 			}
+
+			if ( moment(time[i]).format("w") == now.format("w")){
+				commitsWeekly +=1
+			}
+
 		}
 	})
-	.success(function() {
-		$("#commits-today").append(commitsToday)
+	.complete(function() {
+		$("#commits-today").append(commitsToday);
+		$("#commits-weekly").append(commitsWeekly);
 
 		for (var i = 0; i < 3; i++) {
 			$("#commit-messages").append(messages[i] + " — " + getPrettyTime(time[i]) + "<br />");
